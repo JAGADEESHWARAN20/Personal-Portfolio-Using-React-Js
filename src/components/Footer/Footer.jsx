2482c82c42bb7438fcf91b23fc0a0ca4141635d7
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2023-07-04 09:57:50 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
 
-    <section className='lg:w-full mobile2:w-auto md:w-auto small-devices:w-auto  dark:bg-slate-900 small-devices:flex small-devices:flex-col bg-transparent items-center flex flex-row justify-center h-[10px] '>
+    <section className='lg:w-full mobile2:w-auto md:w-auto small-devices:w-auto bg-white  dark:bg-slate-900 small-devices:flex small-devices:flex-col bg-transparent items-center flex flex-row justify-center h-[10px] '>
       <div className='lg:w-full   small-devices:flex-col  list-none flex lg:flex-row gap-5 justify-evenly'>
          <div className='flex flex-row gap-3 items-center'>
          <h1 class='text-[12px] text-black dark:text-white font-[poppins]'>Developed by <a href="https://github.com/JAGADEESHWARAN20">@jagadeesh</a> </h1>
