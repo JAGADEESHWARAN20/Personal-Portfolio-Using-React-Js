ca0fbe9dd00f2d3b50dc466a73ba3d7da4f8e25b
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2023-10-09 23:54:57 +0530

    Update TermsAndConditions.jsx

--- src/components/UI/TermsAndConditions.jsx
+++ src/components/UI/TermsAndConditions.jsx
@@ -21,12 +21,62 @@ const TermsAndConditions = () => {
           relationship with you in relation to this website.
         </p>
 
-        {/* ... Add more content as needed ... */}
+        <p className="mb-4">
+          The term 'Jagadeesh - Web Developer' or 'us' or 'we' refers to the owner of the website whose registered/operational
+          office is No 10 Thirunavukarasu street, Annai Indra nagar, selaiyur, Chitlapakkam, Chennai Chennai TAMIL NADU 600073.
+          The term 'you' refers to the user or viewer of our website.
+        </p>
 
         <p className="mb-4">
-          We as a merchant shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly
-          out of the decline of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit
-          mutually agreed by us with our acquiring bank from time to time.
+          The use of this website is subject to the following terms of use:
+        </p>
+
+        <p className="mb-4">
+          The content of the pages of this website is for your general information and use only. It is subject to change without notice.
+        </p>
+
+        <p className="mb-4">
+          Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness
+          or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge
+          that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies
+          or errors to the fullest extent permitted by law.
+        </p>
+
+        <p className="mb-4">
+          Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall
+          be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.
+        </p>
+
+        <p className="mb-4">
+          This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout,
+          look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these
+          terms and conditions.
+        </p>
+
+        <p className="mb-4">
+          All trademarks reproduced in this website which are not the property of, or licensed to, the operator are acknowledged on the website.
+        </p>
+
+        <p className="mb-4">
+          Unauthorized use of this website may give rise to a claim for damages and/or be a criminal offense.
+        </p>
+
+        <p className="mb-4">
+          From time to time this website may also include links to other websites. These links are provided for your convenience to provide further information.
+        </p>
+
+        <p className="mb-4">
+          You may not create a link to this website from another website or document without Jagadeesh - Web Developer’s prior written consent.
+        </p>
+
+        <p className="mb-4">
+          Your use of this website and any dispute arising out of such use of the website is subject to the laws of India or other regulatory authority.
+        </p>
+
+        <p className="mb-4">
+          We as a merchant shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline
+          of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit mutually agreed by us with our acquiring
+          bank from time to time.
         </p>
       </div>
     </div>
