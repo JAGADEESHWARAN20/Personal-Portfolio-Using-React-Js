b0dc13725b441ddeaf8e2f51cd13cc1ab15882ba
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2023-10-09 18:34:43 +0530

    Create PrivacyPolicy.jsx

---
+++ src/components/UI/PrivacyPolicy.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+
+  return (
+    <div className="bg-gray-100 min-h-screen p-8">
+      <div className="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
+        <p className="text-sm text-gray-600 mb-4">
+          Last updated on Oct 9th, 2023
+        </p>
+        <p>
+          This privacy policy sets out how Jagadeesh - Web Developer uses and
+          protects any information that you give Jagadeesh - Web Developer when
+          you use this website.
+        </p>
+        <p>
+          Jagadeesh - Web Developer is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, and then you can be assured that it will only be used in accordance with this privacy statement.
+        </p>  
+        <p>
+          Jagadeesh - Web Developer may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.
+        </p>
+        <p>
+          We may collect the following information:
+          <ul>
+            <li>
+              Name and job title
+            </li>
+            <li>
+              Contact information including email address
+            </li>
+            <li>
+              Demographic information such as postcode, preferences and interests
+            </li>
+            <li>
+              Other information relevant to customer surveys and/or offers
+            </li>
+          </ul>
+          </p>
+          <p>
+          What we do with the information we gather
+          </p>
+          <p>
+          We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:
+            <ul>
+              <li>
+              Internal record keeping.
+              </li>
+              <li>
+              We may use the information to improve our products and services.
+              </li>
+              <li>
+              We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.
+              </li>
+              <li>
+              From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail. We may use the information to customise the website according to your interests.
+              </li>
+            </ul>
+          </p>
+
+          <h1>Controlling your personal information</h1>
+          <p>
+          You may choose to restrict the collection or use of your personal information in the following ways:
+          </p>
+          <li>
+          whenever you are asked to fill in a form on the website, look for the box that you can click to indicate that you do not want the information to be used by anybody for direct marketing purposes
+          </li>
+
+          <li>
+          if you have previously agreed to us using your personal information for direct marketing purposes, you may change your mind at any time by writing to or emailing us at jagadeeshwaransp5@gmail.com
+          </li>
+
+          <li>
+          We will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties which we think you may find interesting if you tell us that you wish this to happen.
+          </li>
+          <p>
+          If you believe that any information we are holding on you is incorrect or incomplete, please write to or email us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.
+          </p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
+
+
+
+
+
+
+
+
+
+
+
+
+
