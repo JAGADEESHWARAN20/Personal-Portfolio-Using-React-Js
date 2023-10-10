3fdf8f47848028f2f8825bdbbe6e50d53d9d8681
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2023-10-10 21:25:20 +0530

    Update PrivacyPolicy.jsx

--- src/components/UI/PrivacyPolicy.jsx
+++ src/components/UI/PrivacyPolicy.jsx
@@ -1,64 +1,66 @@
 import React from 'react';
+import "./privacypolicy.css"
 
 const PrivacyPolicy = () => {
 
   return (
-    <div className="bg-gray-100 min-h-screen p-8">
-      <div className="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
-        <p className="text-sm text-gray-600 mb-4">
+    <section
+  className={`pt-0 transition-all dark:text-white bg-white duration-300 dark:bg-slate-900 pb-[80px] overflow-y-auto h-[800px]`}
+ id='privacy-policy'>
+      <div className="max-w-5xl mx-auto p-4 sm:p-8">
+        <h1 className="text-4xl font-bold mb-4">Privacy Policy</h1>
+        <p className="text-md text-gray-600 dark:text-white mb-4">
           Last updated on Oct 9th, 2023
         </p>
-        <p>
+        <p className='text-md'>
           This privacy policy sets out how Jagadeesh - Web Developer uses and
           protects any information that you give Jagadeesh - Web Developer when
           you use this website.
         </p>
-        <p>
+        <p className='text-md'>
           Jagadeesh - Web Developer is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, and then you can be assured that it will only be used in accordance with this privacy statement.
         </p>  
-        <p>
+        <p className='pb-2 text-md'>
           Jagadeesh - Web Developer may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.
         </p>
         <p>
-          We may collect the following information:
-          <ul>
-            <li>
+          <span className='text-xl text-md'>We may collect the following information:</span>
+          <ul className='ml-6 mt-2 pb-3 text-md'>
+            <li className='list-disc'>
               Name and job title
             </li>
-            <li>
+            <li className='list-disc'>
               Contact information including email address
             </li>
-            <li>
+            <li className='list-disc'>
               Demographic information such as postcode, preferences and interests
             </li>
-            <li>
+            <li className='list-disc'>
               Other information relevant to customer surveys and/or offers
             </li>
           </ul>
           </p>
-          <p>
+          <p className='text-xl'>
           What we do with the information we gather
           </p>
-          <p>
+          <p className='py-2 '>
           We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:
-            <ul>
-              <li>
+            <ul className='ml-6 mt-2 pb-3'>
+              <li className='list-disc'>
               Internal record keeping.
               </li>
-              <li>
+              <li className='list-disc'>
               We may use the information to improve our products and services.
               </li>
-              <li>
+              <li className='list-disc'>
               We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.
               </li>
-              <li>
+              <li className='list-disc'>
               From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail. We may use the information to customise the website according to your interests.
               </li>
             </ul>
           </p>
-
-          <h1>Controlling your personal information</h1>
+          <h1 className='py-3 text-xl'>Controlling your personal information</h1>
           <p>
           You may choose to restrict the collection or use of your personal information in the following ways:
           </p>
@@ -69,7 +71,6 @@ const PrivacyPolicy = () => {
           <li>
           if you have previously agreed to us using your personal information for direct marketing purposes, you may change your mind at any time by writing to or emailing us at jagadeeshwaransp5@gmail.com
           </li>
-
           <li>
           We will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties which we think you may find interesting if you tell us that you wish this to happen.
           </li>
@@ -77,7 +78,8 @@ const PrivacyPolicy = () => {
           If you believe that any information we are holding on you is incorrect or incomplete, please write to or email us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.
           </p>
       </div>
-    </div>
+   
+   </section>
   );
 };
 
